833c8fddd14d56b2cea2fb17529428d8b3aeef85
braum852 <julian.teo59@yahoo.com>
2022-03-01 16:15:41 +1000

    Done

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,13 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+courses = doc.css(".heading-40-semibold").text
+
+    courses.each do |course|
+    course.text.strip
+    end
+
+
+#ruby lib/scraper.rb
